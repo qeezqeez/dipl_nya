f507fa1089228322874a74aa5a6842d3cdc4908c
qeez <qeezqeezqeez@inbox.ru>
2024-09-23 19:07:08 +0300

    Now dictionaries should work on windows.

--- lua/dipl_dicts/init.lua
+++ lua/dipl_dicts/init.lua
@@ -1,7 +1,7 @@
 local DICTIONARIES = {}
 local files = vim.api.nvim_get_runtime_file("lua/dipl_dicts/*.lua", true)
 for i = 1, #files do
-  local mod_name = files[i]:match("[^%/]*$"):match("[^%.]*")
+  local mod_name = files[i]:match("[^%/%\\]*$"):match("[^%.]*")
   local dict, dict_name
   if mod_name ~= "init" then
     dict, dict_name = unpack(require("dipl_dicts." .. mod_name))
