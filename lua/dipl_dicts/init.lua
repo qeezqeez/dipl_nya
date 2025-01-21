4b2d5fc9608e5db486e769b31888e203b48e9463
qeez <qeezqeezqeez@inbox.ru>
2025-01-21 15:45:17 +0300

    Work in progress.

--- lua/dipl_dicts/init.lua
+++ lua/dipl_dicts/init.lua
@@ -1,5 +1,35 @@
 local DICTIONARIES = {}
 
+---@class Word
+local Word = {
+  Translations = {}
+}
+
+---@param key string -- Key for word.
+---@param translate string -- Word translate.
+---@param colour string -- Colour for highlight translate in "#000000" format.
+---@param comment string -- Comment for translate.
+-- Add new translate to the word in dictionary.
+function Word:add_translate(key, translate, colour, comment)
+  local val = {}
+  val.key = key or "default_key"
+  val.translate = translate or "default_translate"
+  val.colour = colour or "#ff0000"
+  val.comment = comment or "default_comment"
+
+  table.insert(self.Translations, val)
+end
+
+---@return Word
+function Word:new()
+  local instance = {}
+  for k, v in pairs(self) do
+    instance[k] = v
+  end
+
+  return instance
+end
+
 ---@class Dictionary
 local Dictionary = {}
 
@@ -7,8 +37,19 @@ function Dictionary:set_name(name)
   self.Name = name
 end
 
+---@return string
 function Dictionary:get_name()
   return self.Name
+end
+
+---@param colour string -- Colour in "#000000" format.
+function Dictionary:set_colour(colour)
+  self.Colour = colour
+end
+
+---@return string
+function Dictionary:get_colour()
+  return self.Colour
 end
 
 function Dictionary:add_word(word)
